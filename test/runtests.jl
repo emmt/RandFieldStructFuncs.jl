40d0e63eb58a31079e5aa2e05326d13cc986845c
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2023-03-28 16:53:42 +0200

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,35 @@
+module TestingStructureFunctions
+
 using StructureFunctions
-using Test
+using Test, Statistics
 
 @testset "StructureFunctions.jl" begin
-    # Write your tests here.
+    f = KolmogorovStructFunc(1.2)
+    let f32 = KolmogorovStructFunc{Float32}(f)
+        z1 = CartesianIndex(0,0)
+        z2 = (zero(BigFloat), zero(Int))
+        @test f isa KolmogorovStructFunc{Float64}
+        @test f32 isa KolmogorovStructFunc{Float32}
+        @test f(z1) === zero(Float64)
+        @test f(z2) === zero(Float64)
+        @test f32(z1) === zero(Float32)
+        @test f32(z2) === zero(Float32)
+    end
+    S = [sqrt(x^2 + y^2) < 4.5 for x in -6:6, y in -6:6]
+    n = length(S)
+    nnz = count(x -> x > zero(x), S)
+    @test sum(StructureFunctions.normalize_support(Float64, S)) ≈ 1
+    C = cov(f, S)
+    @test size(C) == (n, n)
+    @test C == C'
+    C = cov(f, S, 0.1)
+    @test size(C) == (n, n)
+    @test C == C'
+    C = cov(f, S; shrink=true)
+    @test size(C) == (nnz, nnz)
+    @test C == C'
 end
+
+end # module
+
+nothing
